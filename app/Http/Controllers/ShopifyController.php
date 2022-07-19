4c84e3cc83510f7d4357e11642e14211dc207cb4
ricky.galang@healthfirst.com.ph <ricky.galang@healthfirst.com.ph>
2022-07-19 09:34:14 +0800

    change in shopify controller

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
 
 class ShopifyController extends Controller
 {
     //
     public function sampleAPI () {
-        $endpoint = 'https://api.publicapis.org/entries';
-        $client = new \GuzzleHttp\Client();
-
-        $response = $client->request('GET', $endpoint);
-        $statusCode = $response->getStatusCode();
-        return $result = json_decode($response->getBody(), true);
+        $http = new Client([
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ]
+        ]);
+        try {
+            $response = $http->get('https://api.publicapis.org/entries');
+            $result = json_decode((string) $response->getBody(), true);
+            return $result;
+        } catch (RequestException $e) {
+            $errors = $e->getResponse();
+            return $errors = json_decode((string) $errors->getBody(), true);
+        }
     }
 }
